c2afea4fcad7b39f30b118ddc0affe4bd420d172
xavier jouppe <xajouppe@gmail.com>
2024-09-20 22:07:29 +0200

    feat: speed up

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
-const speed : float = 10.0
+var speed : float = 3.0
 var screen_size : Vector2i
+var started = false
+
 
 func _ready():
 	screen_size = get_window().size
 
 func _process(delta: float) -> void:
-	$Player.position.x += speed
-	$Camera2D.position.x += speed
+	if Input.is_action_just_pressed("ui_accept"):
+		started= true
+
+	if started:
+		speed+= delta
+		$Player.position.x += speed
+		$Camera2D.position.x += speed
 	
-	#update ground position
-	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+		#update ground position
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
 		
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,18 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if not get_parent().started:
+		$AnimatedSprite2D.play("idle")
+	else: 
+		$AnimatedSprite2D.play("run")
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
 
 
 	move_and_slide()
