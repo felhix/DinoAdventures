aee3857b3f9b939640747e85922f56f291c2d7bd
felhix <felix.gaude@gmail.com>
2024-09-21 17:03:10 +0200

    death is ready

---
+++ scenes/objects/obstacle.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("obstacles")
+
+func _on_body_entered(body):
+	if body.is_in_group("players"):
+		body.take_damage()
