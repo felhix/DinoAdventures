5179615ce56f9f2a2c3e30716f0655c24acc23c8
xavier jouppe <xajouppe@gmail.com>
2024-09-21 17:06:49 +0200

    feat: menuing

---
+++ scenes/Store/store.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# this is the global store
+
+const ACharScenes = [
+	preload("res://scenes/player/ground_player/bear/bear.tscn"),
+	preload("res://scenes/player/ground_player/fox/fox.tscn"),
+	preload("res://scenes/player/ground_player/adult_deer/adult_deer.tscn"),
+]
+const BCharScenes = [
+	preload("res://scenes/player/ground_player/wolf/wolf.tscn"),
+	preload("res://scenes/player/ground_player/boar/boar.tscn"),
+	preload("res://scenes/player/flying_player/pigeon/pigeon.tscn")
+]
--- scenes/UI/menu/menu.gd
+++ scenes/UI/menu/menu.gd
@@ -1,37 +1,69 @@
 extends Node
 
-
-const ACharScenes = [
-	preload("res://scenes/player/ground_player/bear/bear.tscn"),
-	preload("res://scenes/player/ground_player/fox/fox.tscn"),
-	preload("res://scenes/player/ground_player/adult_deer/adult_deer.tscn"),
-]
-const BCharScenes = [
-	preload("res://scenes/player/flying_player/bird/bird.tscn"),
-	preload("res://scenes/player/flying_player/pigeon/pigeon.tscn")
-]
-
-func addPlayer(anchor, player, index):
-	var char = player.instantiate()
-	anchor.add_sibling(char)
-	char.runAnim()
-	
-	char.scale = Vector2(5, 5)
-	char.position.x = anchor.position.x + 500 + index*300
-	char.position.y = anchor.position.y + 50
+@onready var labelPlayerA = get_node('PlayerA2')
+@onready var labelPlayerB = get_node('PlayerB2')
+@onready var Store = get_node("/root/Store")
 
 var started = false
 
-func _ready():
-	var labelPlayerA = get_node('PlayerA2')
-	var labelPlayerB = get_node('PlayerB2')
+var playerAList = []
+var playerBList = []
+
+var selectedPlayerA = 0
+var selectedPlayerB = 0
+
+func addPlayer(anchor, player, index):
+	anchor.add_sibling(player)
+	player.runAnim()
+	
+	player.scale = Vector2(5, 5)
+	player.position.x = anchor.position.x + 500 + index*300
+	player.position.y = anchor.position.y + 50
+
+
+func _ready():	
+	var ACharScenes = Store.ACharScenes
+	var BCharScenes = Store.BCharScenes
 	
 	for i in range(0,len(ACharScenes)):
-		addPlayer(labelPlayerA,ACharScenes[i], i)
+		var player = ACharScenes[i].instantiate()
+		addPlayer(labelPlayerA, player, i)
+		playerAList.append(player)
 	
 	for j in range(0,len(BCharScenes)):
-		addPlayer(labelPlayerB,BCharScenes[j], j)
+		var player = BCharScenes[j].instantiate()
+		addPlayer(labelPlayerB,player, j)
+		playerBList.append(player)
 
 func _input(event):
 	if event.is_action_pressed("Start"):
 		get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+	
+	# select player A
+	if event.is_action_pressed("next_player_A"):
+		popPlayerA(1)
+		
+	if event.is_action_pressed("previous_player_A"):
+		popPlayerA(-1)
+		
+	# select player B
+	if event.is_action_pressed("next_player_B"):
+		popPlayerB(1)
+		
+	if event.is_action_pressed("previous_player_B"):
+		popPlayerB(-1)
+		
+func popPlayerA(i:int):
+	selectedPlayerA = (selectedPlayerA+i) % len(Store.ACharScenes)
+	updateList(playerAList, i)
+
+func popPlayerB(i:int):
+	selectedPlayerB = (selectedPlayerB+i) % len(Store.BCharScenes)
+	updateList(playerBList, i)
+	
+func updateList(list, i):
+	for u in range(0, len(list)):
+		if i==u:
+			list[i].add_child()
+		else:
+			list[i].select(false)
