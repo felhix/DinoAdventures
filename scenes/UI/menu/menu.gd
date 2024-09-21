1866f21d31d37e52702243538fb798fccda18c57
xavier jouppe <xajouppe@gmail.com>
2024-09-21 16:08:16 +0200

    feat: add chars to menu

--- scenes/UI/menu/menu.gd
+++ scenes/UI/menu/menu.gd
@@ -11,23 +11,26 @@ const BCharScenes = [
 	preload("res://scenes/player/flying_player/pigeon/pigeon.tscn")
 ]
 
+func addPlayer(anchor, player, index):
+	var char = player.instantiate()
+	anchor.add_sibling(char)
+	char.runAnim()
+	
+	char.scale = Vector2(5, 5)
+	char.position.x = anchor.position.x + 500 + index*300
+	char.position.y = anchor.position.y + 50
 
 var started = false
 
 func _ready():
-	var containerA = get_node('.')
-	
-	print(ACharScenes)
-	
+	var labelPlayerA = get_node('PlayerA2')
+	var labelPlayerB = get_node('PlayerB2')
 	
 	for i in range(0,len(ACharScenes)):
-		var char = ACharScenes[i].instantiate()
-		char.runAnim()
-		containerA.add_child(char)
-		char.position.x = containerA.position.x
-		char.position.y = containerA.position.y
-		
-		print(char)
+		addPlayer(labelPlayerA,ACharScenes[i], i)
+	
+	for j in range(0,len(BCharScenes)):
+		addPlayer(labelPlayerB,BCharScenes[j], j)
 
 func _input(event):
 	if event.is_action_pressed("Start"):
