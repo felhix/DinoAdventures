e591014451df6f7318c53c9c172df853bd55ff00
Douglas DUTEIL <douglasduteil@gmail.com>
2024-11-24 19:40:22 +0100

    feat: pigeon are friendlty

--- scenes/player/flying_player/pigeon/pigeon.gd
+++ scenes/player/flying_player/pigeon/pigeon.gd
@@ -2,10 +2,6 @@ class_name Pigeon extends FlyingPlayer
 
 const MIN_SPEED = 500
 
-func _on_body_entered(body):
-	if body.is_in_group("players"):
-		body.take_damage()
-		
 func _process(delta: float) -> void:
 	position.x -= delta * MIN_SPEED * scale.x
-	position.y += randi_range(-2, 2) + cos(position.x)
+	position.y += randi_range(-2, 1) + cos(position.x)
