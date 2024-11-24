908330f9cc4767aaf65b099171d0e10ee3927211
xavier jouppe <xajouppe@gmail.com>
2024-11-24 17:48:47 +0100

    stuff

--- scenes/Store/store.gd
+++ scenes/Store/store.gd
@@ -4,12 +4,12 @@ extends Node
 
 const ACharScenes = [
 	preload("res://scenes/player/ground_player/bear/bear.tscn"),
-	preload("res://scenes/player/ground_player/fox/fox.tscn"),
 	preload("res://scenes/player/ground_player/adult_deer/adult_deer.tscn"),
+	preload("res://scenes/player/ground_player/fox/fox.tscn"),
 ]
 const BCharScenes = [
-	preload("res://scenes/player/ground_player/wolf/wolf.tscn"),
 	preload("res://scenes/player/ground_player/boar/boar.tscn"),
+	preload("res://scenes/player/ground_player/wolf/wolf.tscn"),
 	preload("res://scenes/player/ground_player/bunny/bunny.tscn")
 ]
 
