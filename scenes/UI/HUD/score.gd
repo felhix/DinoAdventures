ccf732947c866484e569b95b81f84024d646b8b7
Douglas DUTEIL <douglasduteil@gmail.com>
2024-11-24 23:13:47 +0100

    refactor: move score logic to score ui

---
+++ scenes/UI/HUD/score.gd
@@ -0,0 +1,13 @@
+class_name ScoreUI extends CanvasLayer
+
+@onready var DistanceValueLabel: Label = $DistanceValueLabel
+@onready var TimeLeftValueLabel: Label = $TimeLeftValueLabel
+
+var time_left: int = 0
+
+func _process(delta: float) -> void:
+	var time_str = time_left / 1000
+	var s =str(time_left / 1000)
+	var ms = str(int(time_left % 1000)/10)
+	var fucking_zero = '0' if len(ms) == 1 else ''
+	TimeLeftValueLabel.text = s+':'+fucking_zero+ms
--- scenes/level/entity_level.gd
+++ scenes/level/entity_level.gd
@@ -11,8 +11,6 @@ var started: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Music.play()
-	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,18 +19,7 @@ func _process(delta: float) -> void:
 		time_left -= delta*1000
 		BackDayNightColor.color = Color("#00246e").lerp(Color("#FFF"), time_left / LEVEL_TIME_LEFT)
 		GroundDayNightColor.color = Color("#86d4ff").lerp(Color("#FFF"), time_left / LEVEL_TIME_LEFT)
-		show_score()
+		Score.time_left = time_left
 
 	if started == false and Input.is_action_just_pressed("ui_accept"):
 		started= true
-
-
-###
-
-
-func show_score():
-	var time_str = int(time_left) / 1000
-	var s =str(int(time_left) / 1000)
-	var ms = str(int(int(time_left) % 1000)/10)
-	var fucking_zero = '0' if len(ms) == 1 else ''
-	Score.TimeLeftValueLabel.text = s+':'+fucking_zero+ms
